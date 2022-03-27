96c8c155e499c5d391d9e5c59515671a61483ef4
Max Lütkemeyer <max.luetkemeyer@uni-muenster.de>
2022-03-27 18:47:25 +0200

    basic structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2022": true,
+        "jest": true,
+        "mongo": true,
+    },
+    "extends": [
+        "google",
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+    },
+    "rules": {
+        "indent": ["error", 4],
+        "quotes": ["error", "double"],
+    },
+};
--- index.js
+++
@@ -1,9 +0,0 @@
-// mongodb + node
-// node alpine
-// linter
-// workflows
-// docker hub connection
-
-const i = 5;
-
-console.log(i);
